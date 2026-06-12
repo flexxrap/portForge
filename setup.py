31359e893a63b47449ea581dd765830bd6c3131c
flexxrap <flexxrap@users.noreply.github.com>
2026-06-12 17:00:11 +0300

    build: Add package setup and version info
    Added setup.py for package distribution with entry point configuration. Added version info to src/__init__.py. Package can now be installed with pip install .

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh.readlines()]
+
+setup(
+    name="port-scanner",
+    version="1.0.0",
+    author="flexxrap",
+    description="Network Port Scanner with multi-threading and service detection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.11",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "port-scanner=src.scanner:main",
+        ],
+    },
+)
--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,4 @@
 """Port Scanner - Network scanning tool for port enumeration and service detection."""
+
+__version__ = "1.0.0"
+__author__ = "flexxrap"
